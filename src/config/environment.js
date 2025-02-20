d602c56d874f872b641645e877deb025a1fac5f1
Stevam Reis <stevamreis@gmail.com>
2025-02-20 20:33:29 -0300

    [0001] - criacao de env

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = "https://onetalk-service.fly.dev";
-const API_BASE_URL_LOCAL = "http://localhost:8080";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const ENDPOINTS = {
   FRIENDS_LIST: `${API_BASE_URL}/friendship`,
